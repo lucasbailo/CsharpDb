86974ad2e05f47c6233459a71657644f6fedad07
Lucas Bailo <lucasebailo@gmail.com>
2025-06-08 20:33:04 -0300

    Adding conection strings and creating ArtistaDAL

---
+++ ScreenSound/Banco/Conection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using ScreenSound.Modelos;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ScreenSound.Banco
+{
+    internal class Conection
+    {
+
+        private string conectionString = "Data Source=DEVFRONT;Integrated Security=True;Initial Catalog=ScreenSoundDb;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(conectionString);
+        }
+    }
+}
