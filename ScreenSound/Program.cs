860f312184d5fb54204683c050acab3001297773
Lucas Bailo <lucasebailo@gmail.com>
2025-06-15 16:39:13 -0300

    Updating Program.cs

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -2,41 +2,8 @@
 using ScreenSound.Menus;
 using ScreenSound.Modelos;
 
-
-try
-{
-    var context = new ScreenSoundContext();
-    var artistaDAL = new ArtistaDAL(context);
-
-
-    var novoArtista = new Artista("Zeca Pagodinho", "Esse cara é gente boa") { Id = 4};
-
-    //artistaDAL.Adicionar(novoArtista);
-    //artistaDAL.Deletar(novoArtista);
-    var listaArtistas = artistaDAL.Listar();
-
-    var artistaRecuperado = artistaDAL.RecuperarPeloNome("Zeca Pagodinho");
-    Console.WriteLine(artistaRecuperado);
-
-    foreach ( var artista in listaArtistas )
-    {
-        Console.WriteLine(artista);
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-return;
-
-
-Artista ira = new Artista("Ira!", "Banda Ira!");
-Artista beatles = new("The Beatles", "Banda The Beatles");
-
-Dictionary<string, Artista> artistasRegistrados = new();
-artistasRegistrados.Add(ira.Nome, ira);
-artistasRegistrados.Add(beatles.Nome, beatles);
+var context = new ScreenSoundContext();
+var artistaDAL = new ArtistaDAL(context);
 
 Dictionary<int, Menu> opcoes = new();
 opcoes.Add(1, new MenuRegistrarArtista());
