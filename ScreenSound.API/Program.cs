0c6d3bfa038711737419408ba223f3c16243c031
Lucas Bailo <lucasebailo@gmail.com>
2025-09-04 14:09:24 -0300

    create: atividade de criar o CRUD de Musica

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -4,10 +4,15 @@ using ScreenSound.Modelos;
 using System.Data.SqlTypes;
 using System.Text.Json.Serialization;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+builder.Services.AddTransient<DAL<Musica>>();
 
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
@@ -58,6 +63,52 @@ app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista art
     artistaAtualizar.FotoPerfil = artista.FotoPerfil;
 
     dal.Atualizar(artistaAtualizar);
+    return Results.Ok();
+});
+
+app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
+{
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Musicas/{nome}", ([FromServices] DAL<Musica> dal, string nome) =>
+{
+    var musica = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(musica);
+
+});
+
+app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
+{
+    dal.Adicionar(musica);
+    return Results.Ok();
+});
+
+app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) => {
+    var musica = dal.RecuperarPor(a => a.Id == id);
+    if (musica is null)
+    {
+        return Results.NotFound();
+    }
+    dal.Deletar(musica);
+    return Results.NoContent();
+
+});
+
+app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) => {
+    var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
+    if (musicaAAtualizar is null)
+    {
+        return Results.NotFound();
+    }
+    musicaAAtualizar.Nome = musica.Nome;
+    musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
+
+    dal.Atualizar(musicaAAtualizar);
     return Results.Ok();
 });
 
